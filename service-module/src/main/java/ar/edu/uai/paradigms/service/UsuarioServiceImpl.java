87b0c029d6e0c5bcca777e7fa8140c562af9614d
Matias Barrera <matiiasbarrera6@gmail.com>
2015-05-16 03:07:23 -0300

    aca borre algo que se me habia generado por error

--- service-module/src/main/java/ar/edu/uai/paradigms/service/UsuarioServiceImpl.java
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/UsuarioServiceImpl.java
@@ -1,12 +1,10 @@
-package ar.edu.uai.paradigms.service.impl;
+package ar.edu.uai.paradigms.service;
 
 
 
 
 import ar.edu.uai.model.Usuario;
-
 import ar.edu.uai.paradigms.dao.UsuarioDAO;
-import ar.edu.uai.paradigms.service.UsuarioService;
 
 import org.springframework.transaction.annotation.Transactional;
 
