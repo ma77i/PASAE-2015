e85cdceaf071c5b0912e6c7a16f4e4558c5fc29a
Ezequiel <Ezequiel@NOTEBOOK-PC>
2015-05-21 19:11:08 -0300

    asd

---
+++ web-module/src/main/java/ar/edu/uai/paradigms/controller/EmpleadoController.java
@@ -0,0 +1,49 @@
+package ar.edu.uai.paradigms.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ar.edu.uai.model.Espectador;
+import ar.edu.uai.paradigms.dto.EmpleadoDTO;
+import ar.edu.uai.paradigms.dto.EspectadorDTO;
+import ar.edu.uai.paradigms.service.EmpleadoService;
+import ar.edu.uai.paradigms.service.EspectadorService;
+import ar.edu.uai.paradigms.translator.EmpleadoTranslator;
+import ar.edu.uai.paradigms.translator.EspectadorTranslator;
+
+public class EmpleadoController {
+	
+	
+	public EmpleadoController(
+			EmpleadoService empleadoService,
+			EmpleadoTranslator empleadoTranslator) {
+		super();
+		this.empleadoService = empleadoService;
+		this.empleadoTranslator = empleadoTranslator;
+	}
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(EspectadorController.class);
+
+	private EmpleadoService empleadoService;
+
+	private EmpleadoTranslator empleadoTranslator;
+
+	@RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody EmpleadoDTO createEmpleado(@RequestBody EmpleadoDTO empleadoDTO) {
+		LOGGER.debug("Received DTO: " + empleadoDTO);
+		return this.empleadoTranslator.translateToDTO(this.empleadoService
+                .saveEspectador(this.empleadoTranslator.translate(empleadoDTO)));
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{identifier}")
+	public @ResponseBody EmpleadoDTO getEspectador(@PathVariable long identifier) {
+		return this.empleadoTranslator.translateToDTO(this.empleadoService.retrieveEspectador(identifier));
+	}
+
+}
