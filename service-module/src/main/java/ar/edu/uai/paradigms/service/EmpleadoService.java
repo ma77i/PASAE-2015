5695765f04bf57e010d49573cbd2e272233d95b1
Ezequiel <Ezequiel@NOTEBOOK-PC>
2015-05-21 19:11:08 -0300

    asd

---
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/EmpleadoService.java
@@ -0,0 +1,13 @@
+package ar.edu.uai.paradigms.service;
+
+import ar.edu.uai.model.Empleado;
+import ar.edu.uai.model.Espectador;
+import ar.edu.uai.model.Usuario;
+import ar.edu.uai.paradigms.dao.UsuarioDAO;
+
+public class EmpleadoService extends UsuarioService <T> {
+	
+
+
+
+}
