863727a7b0c0dd1c87fd8e8815e5019b2dca91b5
Ezequiel <Ezequiel@NOTEBOOK-PC>
2015-05-21 19:59:08 -0300

    asd

--- service-module/src/main/java/ar/edu/uai/paradigms/service/EmpleadoService.java
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/EmpleadoService.java
@@ -1,11 +1,11 @@
 package ar.edu.uai.paradigms.service;
 
-import ar.edu.uai.model.Empleado;
-import ar.edu.uai.model.Espectador;
-import ar.edu.uai.model.Usuario;
-import ar.edu.uai.paradigms.dao.UsuarioDAO;
+public class EmpleadoService <T> extends UsuarioService <T> {
 
-public class EmpleadoService extends UsuarioService <T> {
+	public EmpleadoService(T usuarioDAO) {
+		super(usuarioDAO);
+		// TODO Auto-generated constructor stub
+	}
 	
 
 
