a3a59defc892f06c584cddcde2b0aaf796ade310
Matias Barrera <matiiasbarrera6@gmail.com>
2015-05-22 17:15:30 -0300

    zzz

--- service-module/src/main/java/ar/edu/uai/paradigms/service/EmpleadoService.java
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/EmpleadoService.java
@@ -1,11 +1,17 @@
 package ar.edu.uai.paradigms.service;
 
-public class EmpleadoService <T> extends UsuarioService <T> {
 
-	public EmpleadoService(T usuarioDAO) {
+import ar.edu.uai.model.Usuario;
+import ar.edu.uai.paradigms.dao.UsuarioDAO;
+
+public class EmpleadoService extends  UsuarioService<Usuario>{
+
+	public EmpleadoService(UsuarioDAO usuarioDAO) {
 		super(usuarioDAO);
 		// TODO Auto-generated constructor stub
 	}
+
+	
 	
 
 
--- web-module/src/main/java/ar/edu/uai/paradigms/controller/AdministradorController.java
+++ web-module/src/main/java/ar/edu/uai/paradigms/controller/AdministradorController.java
@@ -3,9 +3,16 @@ package ar.edu.uai.paradigms.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ar.edu.uai.model.Administrador;
+import ar.edu.uai.model.Empleado;
+import ar.edu.uai.paradigms.dto.AdministradorDTO;
+import ar.edu.uai.paradigms.dto.EmpleadoDTO;
 import ar.edu.uai.paradigms.service.AdministradorService;
 import ar.edu.uai.paradigms.service.EspectadorService;
 import ar.edu.uai.paradigms.translator.AdministradorTranslator;
@@ -29,5 +36,20 @@ public class AdministradorController {
 	private AdministradorService administradorService;
 
 	private AdministradorTranslator administradorTranslator;
+	
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody AdministradorDTO createAdministrador(@RequestBody AdministradorDTO administradorDTO) {
+		LOGGER.debug("Received DTO: " + administradorDTO);
+		return this.administradorTranslator.translateToDTO((Administrador) this.administradorService
+                .saveUsuario(this.administradorTranslator.translate(administradorDTO)));
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{identifier}")
+	public @ResponseBody AdministradorDTO getAdministrador(@PathVariable long identifier) {
+		return this.administradorTranslator.translateToDTO((Administrador) this.administradorService.retrieveUsuario(identifier));
+	}
+
+	
 	
 }	
