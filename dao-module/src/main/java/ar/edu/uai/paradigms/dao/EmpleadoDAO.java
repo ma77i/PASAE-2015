15dd62214eeacc6a8aaa42ff0bfc6664db97f49f
Matias Barrera <matiiasbarrera6@gmail.com>
2015-05-25 16:51:26 -0300

    cambios en la estructura de los DAOS, ya que habia cosas que se repetian

--- dao-module/src/main/java/ar/edu/uai/paradigms/dao/EmpleadoDAO.java
+++ dao-module/src/main/java/ar/edu/uai/paradigms/dao/EmpleadoDAO.java
@@ -2,6 +2,6 @@ package ar.edu.uai.paradigms.dao;
 
 import ar.edu.uai.model.Empleado;
 
-public interface EmpleadoDAO extends GenericDAO<Empleado> {
+public interface EmpleadoDAO extends UsuarioDAO<Empleado> {
 
 }
