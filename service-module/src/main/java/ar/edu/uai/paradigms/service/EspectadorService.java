0207986b425e26c3fa4eab796c2320c669d68f52
Usuario <Usuario@Usuario-PC>
2015-05-29 21:00:29 -0300

    yyy

--- service-module/src/main/java/ar/edu/uai/paradigms/service/EspectadorService.java
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/EspectadorService.java
@@ -2,6 +2,8 @@ package ar.edu.uai.paradigms.service;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import ar.edu.uai.model.Usuario;
 import ar.edu.uai.model.Venta;
 import ar.edu.uai.paradigms.dao.EspectaculoDAO;
@@ -13,14 +15,20 @@ public class EspectadorService extends UsuarioService<Usuario> {
     
 	
 
-
 	private EspectadorDAO espectadorDAO;  
+
 	
 	
-	
-	public Collection<Venta> listasCompras(){
-		return null;
-		//return espectadorDAO.listarCompras(id);
+
+    public void setEspectadorDAO(EspectadorDAO espectadorDAO) {
+		this.espectadorDAO = espectadorDAO;
+	}
+
+
+
+    public Collection<Venta> listasCompras(long identifier){
+		
+		return espectadorDAO.listarCompras(identifier);
 		
 		
 	}
