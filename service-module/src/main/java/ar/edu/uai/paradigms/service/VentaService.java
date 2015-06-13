aed45aae33eae31d91b575be5eec9c8b9d7f911d
Usuario <Usuario@Usuario-PC>
2015-06-13 18:28:04 -0300

    jj

--- service-module/src/main/java/ar/edu/uai/paradigms/service/VentaService.java
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/VentaService.java
@@ -8,7 +8,7 @@ public interface VentaService {
 	public Venta saveVenta(Venta venta);
 	public Venta retrieveVenta(long identifier);
 	public Collection<Venta> listarVentas();
-	public Collection<Venta> listarComprasDeEspectador(String espectador);
+	public Collection<Venta> listarComprasDeEspectador(long id_espectador);
 	public Float calcularMontoFinal(long id_sector, long id_espectaculo);
 	public Float calcularMontoNeto(long id_sector, long id_espectaculo);
 	public Float aplicarDescuento(long id_espectaculo);
