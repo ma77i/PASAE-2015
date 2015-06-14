56c024f00718fea7da3d1ab997cf926d0de25c6c
Matias <ma77i_lp_90@hotmail.com>
2015-06-14 18:23:08 -0300

    Merge branch 'master' of https://github.com/ma77i/PASAE-2015
    Conflicts:
    	dao-module/src/main/java/ar/edu/uai/paradigms/dao/VentaDAO.java
    	dao-module/src/main/java/ar/edu/uai/paradigms/dao/VentaDAOImplHibernateJPA.java
    	service-module/src/main/java/ar/edu/uai/paradigms/service/VentaService.java

--- dao-module/src/main/java/ar/edu/uai/paradigms/dao/VentaDAO.java
+++ dao-module/src/main/java/ar/edu/uai/paradigms/dao/VentaDAO.java
@@ -7,5 +7,6 @@ import ar.edu.uai.model.Venta;
 public interface VentaDAO extends GenericDAO<Venta> {
 
 	public Collection<Venta> listarComprasDeEspectador(long id_espectador);
-	  
+
+	public Float calcularMontoNeto (long id_sector, long id_espectaculo);
 }
--- dao-module/src/main/java/ar/edu/uai/paradigms/dao/VentaDAOImplHibernateJPA.java
+++ dao-module/src/main/java/ar/edu/uai/paradigms/dao/VentaDAOImplHibernateJPA.java
@@ -15,5 +15,12 @@ public class VentaDAOImplHibernateJPA  extends GenericDaoHibernateJPA<Venta> imp
 	    consulta.setParameter(1,id_espectador);
 	    return consulta.getResultList();
 	}
+    @Override
+	public Float calcularMontoNeto(long id_sector, long id_espectaculo) {
+		    Query consulta=this.entityManager.createQuery("select s.monto from Sector where s.id=? and s.espectaculo.id=?");
+		    consulta.setParameter(1, id_sector);
+		    consulta.setParameter(2, id_espectaculo);
+		    return (Float) consulta.getSingleResult();
+	}
 
 }
