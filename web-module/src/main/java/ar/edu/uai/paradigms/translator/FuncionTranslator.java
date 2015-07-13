494f2bc02e5a55c5889a8a7faada387653cd2706
Matias <ma77i_lp_90@hotmail.com>
2015-07-13 19:16:39 -0300

    arreglado lo de funcion service , equivalente al problema que habia en espectaculoService

--- web-module/src/main/java/ar/edu/uai/paradigms/translator/FuncionTranslator.java
+++ web-module/src/main/java/ar/edu/uai/paradigms/translator/FuncionTranslator.java
@@ -2,19 +2,11 @@ package ar.edu.uai.paradigms.translator;
 
 import ar.edu.uai.model.Funcion;
 import ar.edu.uai.paradigms.dto.FuncionDTO;
-import ar.edu.uai.paradigms.service.EspectaculoService;
 
 public class FuncionTranslator {
-    EspectaculoService espectaculoService;
-
-
-    public void setEspectaculoService(EspectaculoService espectaculoService) {
-        this.espectaculoService = espectaculoService;
-    }
-
 
     public Funcion translate(FuncionDTO funcionDTO) {
-        return new Funcion(funcionDTO.getFecha(), funcionDTO.getHora(), this.espectaculoService.retrieveEspectaculo(funcionDTO.getEspectaculoId()));
+        return new Funcion(funcionDTO.getFecha(), funcionDTO.getHora());
 
     }
 
