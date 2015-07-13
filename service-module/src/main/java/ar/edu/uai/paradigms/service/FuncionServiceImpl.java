65c6f0db98e3f442093bf57751bf52290b083ebb
Matias <ma77i_lp_90@hotmail.com>
2015-07-13 19:16:39 -0300

    arreglado lo de funcion service , equivalente al problema que habia en espectaculoService

--- service-module/src/main/java/ar/edu/uai/paradigms/service/FuncionServiceImpl.java
+++ service-module/src/main/java/ar/edu/uai/paradigms/service/FuncionServiceImpl.java
@@ -1,8 +1,10 @@
 package ar.edu.uai.paradigms.service;
 
+import ar.edu.uai.model.Espectaculo;
 import ar.edu.uai.model.Funcion;
 import ar.edu.uai.paradigms.dao.FuncionDAO;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -11,6 +13,7 @@ public class FuncionServiceImpl implements FuncionService{
 
 	private FuncionDAO funcionDAO;
 
+	private EspectaculoService espectaculoService;
 
 	public FuncionDAO getFuncionDAO() {
 		return funcionDAO;
@@ -22,10 +25,15 @@ public class FuncionServiceImpl implements FuncionService{
 	}
 
 
-	@Override
-	public Funcion saveFuncion(Funcion funcion) {
-		this.agregarFuncionParaEspectaculo(funcion);
-		return funcionDAO.create(funcion);
+	public void setEspectaculoService(EspectaculoService espectaculoService) {
+		this.espectaculoService = espectaculoService;
+	}
+
+	@Transactional
+	public Funcion saveFuncion(Funcion funcion, long espectaculoId) {
+		Espectaculo espectaculo = espectaculoService.retrieveEspectaculo(espectaculoId);
+		this.agregarFuncionParaEspectaculo(funcion, espectaculo);
+		return funcionDAO.update(funcion);
 	}
 
 	@Override
@@ -52,12 +60,13 @@ public class FuncionServiceImpl implements FuncionService{
 	}
 
 	@Override
-	public void agregarFuncionParaEspectaculo(Funcion savedFuncion) {
-		savedFuncion.getEspectaculo().getFunciones().add(savedFuncion);
+	public void agregarFuncionParaEspectaculo(Funcion savedFuncion, Espectaculo espectaculo) {
+		espectaculo.getFunciones().add(savedFuncion);
 	}
 
 	@Override
 	public Collection<Funcion> listarFuncionesDeEspectaculo(long id_espectaculo) {
 		return funcionDAO.listarFuncionesDeEspectaculo(id_espectaculo);
 	}
+
 }
