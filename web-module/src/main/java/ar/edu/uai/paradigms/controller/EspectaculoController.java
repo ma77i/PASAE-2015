c433b89b7d34622e8fb31b8e592f12a6a688b28e
Ezequiel@OmegaPC <collectorsw2004@yahoo.com.ar>
2016-03-24 16:57:05 -0300

    asd

--- web-module/src/main/java/ar/edu/uai/paradigms/controller/EspectaculoController.java
+++ web-module/src/main/java/ar/edu/uai/paradigms/controller/EspectaculoController.java
@@ -79,6 +79,14 @@ public class EspectaculoController {
 		for (Espectaculo e : coleccion) {
 			espectaculos.add(espectaculoTranslator.translateToDTO(e));
 		}
+
+		try {
+			while (true) {
+				Thread.sleep(10000);
+			}//1000 milliseconds is one second.
+		} catch(InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
 		return espectaculos;
 	}
 
